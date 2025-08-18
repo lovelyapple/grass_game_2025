89063e561425ad0f72f2edaa1ecea9cd2dd37fb4
lovelyapple <lovelyappleoffice@gmail.com>
2025-08-19 03:08:56 +0900

    skillのSeとbuttonのSEが実装されるまで

--- Assets/Scripts/Game/FieldSkillAnimationWatcher.cs
+++ Assets/Scripts/Game/FieldSkillAnimationWatcher.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class FieldSkillAnimationWatcher : MonoBehaviour
 {
+    public void PlaySkillSe(SeType seType)
+    {
+        SoundManager.PlayOneShot(seType);
+    }
     public void OnFinished()
     {
         Destroy(gameObject);
